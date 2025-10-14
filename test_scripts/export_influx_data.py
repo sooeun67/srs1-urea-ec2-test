1d1cbe417a3c8c4d915f15ab1d280bbb4d381ef2
“sooeun67” <sooeun67@georgetown.edu>
2025-10-14 02:10:32 +0000

    change DB host from prod to dev

--- test_scripts/export_influx_data.py
+++ test_scripts/export_influx_data.py
@@ -24,7 +24,8 @@ def export_influx_data(
     hours=1,
     output_file=None,
     measurement="SRS1",
-    host="10.238.27.132", # 운영
+    #host="10.238.27.132", # 운영
+    host="10.238.24.150", # 개발 
     port=8086,
     username="read_user",
     password="!Skepinfluxuser25",
